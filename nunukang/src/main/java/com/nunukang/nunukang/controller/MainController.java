4cdc3b483fd26541454c65c2c0f0df1254055443
Junghun Sim <96688359+092600@users.noreply.github.com>
2023-03-02 12:10:58 +0900

    2022 3 2

---
+++ nunukang/src/main/java/com/nunukang/nunukang/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.nunukang.nunukang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String mainPage() {
+        return "pages/main/mainPage";
+    }
+    
+}
---
+++ nunukang/src/test/java/com/nunukang/nunukang/NunukangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunukang.nunukang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunukangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
